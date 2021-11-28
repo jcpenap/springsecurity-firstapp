b5729ead9eb111e1443b5f6c9797b9b541b33c85
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2021-11-28 14:12:59 -0500

    First Commit

---
+++ src/test/java/com/bahrath/spring/security/FirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahrath.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
