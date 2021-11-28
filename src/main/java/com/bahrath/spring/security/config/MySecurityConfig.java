c6f37570ae3e9a2a5d7d25e79e99607fb4f57af2
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2021-11-28 14:22:48 -0500

    Create Custom Security Configuration

---
+++ src/main/java/com/bahrath/spring/security/config/MySecurityConfig.java
@@ -0,0 +1,20 @@
+package com.bahrath.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
