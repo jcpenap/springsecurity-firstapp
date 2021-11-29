215be667706aaf6793a92904b8ec5d17d0a14c2f
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-29 11:30:23 -0500

    Others PasswordEncoders

--- src/test/java/com/bahrath/spring/security/FirstappApplicationTests.java
+++ src/test/java/com/bahrath/spring/security/FirstappApplicationTests.java
@@ -2,12 +2,18 @@ package com.bahrath.spring.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @SpringBootTest
 class FirstappApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testPasswordEncoders() {
+		System.out.println(new BCryptPasswordEncoder().encode("password"));
+		System.out.println(new Pbkdf2PasswordEncoder().encode("password"));
+		System.out.println(new SCryptPasswordEncoder().encode("password"));
 	}
 
 }
