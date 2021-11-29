7be7846bdebb5ff3c362e3a9615a2940426ebdf8
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-29 09:11:35 -0500

    Tweak the PasswordEncoder

--- src/main/java/com/bahrath/spring/security/config/MySecurityConfig.java
+++ src/main/java/com/bahrath/spring/security/config/MySecurityConfig.java
@@ -1,27 +1,30 @@
 package com.bahrath.spring.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
         InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
         UserDetails user = User.withUsername("tom")
                 .password(passwordEncoder.encode("cruise"))
                 .authorities("read").build();
         userDetailsService.createUser(user);
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
@@ -29,4 +32,5 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic();
         http.authorizeRequests().anyRequest().authenticated();
     }
+
 }
